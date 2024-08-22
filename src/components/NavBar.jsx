05d911fc8413ab6bfb1d8b4c0941787e6340d530
Juanco <210373@utxicotepec.edu.mx>
2024-08-22 14:16:49 -0600

    Crea la navegacion y la añade a la pagina principal

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='h-14 w-full bg-gray-100'>
+        <div className='flex justify-center items-center'>
+            <h1 className='font-bold'>Bienvenido</h1>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
